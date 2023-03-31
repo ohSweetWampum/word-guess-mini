45be5ba00de38f303af4adbe334ad305a1c8d51f
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-03-31 17:00:51 -0500

    started html skeleton, made JS global variables

---
+++ script.js
@@ -0,0 +1,8 @@
+// Global variables
+var words = ["JavaScript","html","cascading style sheets","github","command line","Code","computer","element",];
+
+const word = words[Math.floor(Math.random()* words.length)];
+
+var wins = 0;
+var loses = 0;
+
