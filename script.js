9d118e3f37077291c996903c95d7215580a7a9de
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-04-02 18:06:51 -0500

    addded timer

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 // Global variables
 var words = ["JavaScript","html","cascading style sheets","github","command line","Code","computer","element",];
 
-const word = words[Math.floor(Math.random()* words.length)];
-
+var randomlyChooseIndexNumber = words[Math.floor(Math.random()* words.length)];
+var takeCorrespondingElementFromArray = [randomlyChooseIndexNumber];
 var wins = 0;
 var loses = 0;
 
+//timer starts when user presses "start" button
+
+
+
+
+ var countdownClock = document.getElementById("countdownClock");
+ var secondsLeft = 40;
+ 
+ function timeLeftClock(){
+     var timerInterval = setInterval(function(){
+         secondsLeft--;
+         countdownClock.textContent = secondsLeft + " seconds left!";
+         if(secondsLeft === 0){
+             clearInterval(timerInterval);
+    
+         }
+     }, 1000);
+ }
+ 
+     // Add your code here for when the game is over
+ 
+ 
+ document.getElementById("start-timer-now-button").addEventListener("click", function(){
+     timeLeftClock();
+ });
